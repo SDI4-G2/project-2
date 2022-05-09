3179c2c14e74dce63a1801d135f02a8fd1dc6c78
ngchankok <98152804+ngchankok@users.noreply.github.com>
2022-05-09 16:30:14 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,4 @@ const videoRoutes = require("./video.routes");
 app.use(articleRoutes, categoryRoutes, userRoutes, videoRoutes);
 
 module.exports = app;
->>>>>>> ffbc95dcda9602d14d060546c8449b0c9eb05c2b
+
