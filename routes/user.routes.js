69709a785afba61d8e311ec216af569976a606d5
Gladys <98092633+gladysy@users.noreply.github.com>
2022-05-11 12:40:00 +0800

    testing

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,6 +7,10 @@ const userController = new UserController();
 
 const router = require("express").Router();
 
+router.get("/testing", function (req, res) {
+  res.send("testing");
+});
+
 router.post("/login", userController.userControl);
 
 module.exports = router;
