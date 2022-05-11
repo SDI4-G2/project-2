be6c86b63bdee5eea8354352619e0ae0c60ec7fb
Gladys <98092633+gladysy@users.noreply.github.com>
2022-05-11 14:21:09 +0800

    test6

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = require("./routes/index"); // By default, it will look for index.js 
 const PORT = process.env.PORT || 3000;
 const HOST = "127.0.0.1"; //localhost
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server running at http://${HOST}:${PORT} `);
+// app.listen(PORT, HOST, () => {
+//   console.log(`Server running at http://${HOST}:${PORT} `);
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://${PORT} `);
 });
