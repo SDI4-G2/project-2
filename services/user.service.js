4090592f2da001b88e51a6647f953af162366e8e
Gladys <98092633+gladysy@users.noreply.github.com>
2022-05-11 22:22:25 +0800

    add username or email authentication

--- services/user.service.js
+++ services/user.service.js
@@ -1,34 +1,53 @@
 //when successful / no errors
 
 const User = require("../models/User");
+const { Op } = require("sequelize");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs/dist/bcrypt");
 const fs = require("fs");
 const privateKey = fs.readFileSync("./config/jwtRS256.key");
 
 module.exports = {
-  userControl: async (email, password) => {
+  userControl: async (email, password, username) => {
     const result = {
       status: null,
       message: null,
       data: null,
     };
 
-    //email exists??
-    const user = await User.findOne({
-      where: { email: email },
+    //email/username exists??
+    let user = await User.findOne({
+      where: {
+        [Op.or]: [
+          { username: { [Op.eq]: username } },
+          { email: { [Op.eq]: email } },
+        ],
+      },
     });
+    // documentation =
+    // [Op.or]:[
+    // { username: username },
+    // { email: email }, ]
+    // does not work because there is a bug
 
-    if (!user) return res.status(400).send("Email not found");
+    if (!user) {
+      result.status = 400;
+      result.message = "Username or email not found";
+      return result;
+    }
 
     //password is correct?
     const validatePw = await bcrypt.compare(password, user.password);
-    if (!validatePw) return res.status(400).send("Wrong password");
-
+    if (!validatePw) {
+      result.status = 400;
+      result.message = "Wrong password";
+      return result;
+    }
     //create and assign jwt
     const token = jwt.sign(
       {
         email: user.email,
+        username: user.username,
         password: user.password,
         subscription: user.subscription,
         role: user.role,
