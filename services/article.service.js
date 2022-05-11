945663de651132ef8757c932f38936e982ac4c1f
Ewen <ewenlke@hotmail.com>
2022-05-12 00:53:35 +0800

    Updated article put

--- services/article.service.js
+++ services/article.service.js
@@ -1,6 +1,6 @@
 const res = require('express/lib/response');
 const Article = require('../models/Article');
-const User = require('../models/User');
+const Category = require('../models/Category');
 const { subscribe } = require('../routes/article.routes');
 
 module.exports = {
@@ -34,18 +34,39 @@ module.exports = {
     return result;
   },
 
-  add: async (url, free_to_view) => {
+  add: async ({ categoryid, url, free_to_view }) => {
     const result = {
       status: null,
       message: null,
       data: null,
     };
-    const newArticle = Article.build({ url: url, free_to_view: free_to_view });
+
+    const newArticle = Article.build({ categoryid: categoryid, url: url, free_to_view: free_to_view });
     await newArticle.save();
 
     result.status = 200;
     result.message = 'Added successfully';
     result.data = newArticle;
+
+    return result;
+  },
+
+  update: async ({ articleid, categoryid, url, free_to_view }) => {
+    const result = {
+      status: null,
+      message: null,
+      data: null,
+    };
+    const article = await Article.findByPk(articleid);
+
+    article.categoryid = categoryid;
+    article.url = url;
+    article.free_to_view = free_to_view;
+    await article.save();
+
+    result.status = 200;
+    result.message = 'Updated successfully';
+    result.data = article;
 
     return result;
   },
