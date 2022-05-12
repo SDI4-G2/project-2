34c8003548f500ce56afe623d0ccf82c20051850
Ng Chan Kok <ngchankok@gmail.com>
2022-05-12 10:45:15 +0800

    Amended DBConn.js

--- models/DBConn.js
+++ models/DBConn.js
@@ -2,33 +2,33 @@
 const Sequelize = require("sequelize");
 
 // DB Connection Configuration
-// const sequelize = new Sequelize(
-//   "dbcps5l6ufq0bf",
-//   "cohjqehtiqzjtt",
-//   "b34383c894e401052b751949c3dfff47fb5ed71a55f3c9161cbbe85d9fb33e88",
-//   {
-//     host: "ec2-44-196-223-128.compute-1.amazonaws.com",
-//     dialect: "postgres",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//     define: {
-//       timestamps: false,
-//       freezeTableName: true,
-//     },
-//   }
-// );
-
-const sequelize = new Sequelize('m3_project', 'postgres', 'password', {
-  dialect: 'postgres',
-  define: {
+const sequelize = new Sequelize(
+  "dbcps5l6ufq0bf",
+  "cohjqehtiqzjtt",
+  "b34383c894e401052b751949c3dfff47fb5ed71a55f3c9161cbbe85d9fb33e88",
+  {
+    host: "ec2-44-196-223-128.compute-1.amazonaws.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
       timestamps: false,
-      freezeTableName: true
+      freezeTableName: true,
+    },
   }
-});
+);
+
+// const sequelize = new Sequelize('m3_project', 'postgres', 'password', {
+//   dialect: 'postgres',
+//   define: {
+//       timestamps: false,
+//       freezeTableName: true
+//   }
+// });
 
 // Test DB connection
 sequelize
