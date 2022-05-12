9f8ec20a17bd90eef872b80ee58b085f78547f07
Ng Chan Kok <ngchankok@gmail.com>
2022-05-12 13:44:42 +0800

    Fix error;

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const validator = require('../middleware/jwt.middleware');
 
 const CategoryController = require("../controllers/category.controller");
 
