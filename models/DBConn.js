cb8a85bcdc4b3f9b4746da58e6a93d1596e54427
Ewen <ewenlke@hotmail.com>
2022-05-14 18:52:52 +0800

    Test connection

--- models/DBConn.js
+++ models/DBConn.js
@@ -2,13 +2,13 @@
 const Sequelize = require('sequelize');
 
 /// DB Connection Configuration
-// const sequelize = new Sequelize('m3_project', 'postgres', 'password', {
-//   dialect: 'postgres',
-//   define: {
-//       timestamps: false,
-//       freezeTableName: true
-//   }
-// });
+const sequelize = new Sequelize('m3_project', 'postgres', 'password', {
+  dialect: 'postgres',
+  define: {
+    timestamps: false,
+    freezeTableName: true,
+  },
+});
 
 // Test DB connection
 sequelize
