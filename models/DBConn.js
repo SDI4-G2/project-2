a3e6a5c2f44f9b857b62e7d576f7f2477417aa04
Ewen <ewenlke@hotmail.com>
2022-05-14 18:49:28 +0800

    Test removed credentials

--- models/DBConn.js
+++ models/DBConn.js
@@ -1,27 +1,7 @@
 // Import sequelize
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 /// DB Connection Configuration
-const sequelize = new Sequelize(
-  "dbcps5l6ufq0bf",
-  "cohjqehtiqzjtt",
-  "b34383c894e401052b751949c3dfff47fb5ed71a55f3c9161cbbe85d9fb33e88",
-  {
-    host: "ec2-44-196-223-128.compute-1.amazonaws.com",
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    define: {
-      timestamps: false,
-      freezeTableName: true,
-    },
-  }
-);
-
 // const sequelize = new Sequelize('m3_project', 'postgres', 'password', {
 //   dialect: 'postgres',
 //   define: {
@@ -34,10 +14,10 @@ const sequelize = new Sequelize(
 sequelize
   .authenticate()
   .then(() => {
-    console.log("DB Connection Successful");
+    console.log('DB Connection Successful');
   })
   .catch((err) => {
-    console.log("DB Connection Failed", err);
+    console.log('DB Connection Failed', err);
   });
 
 module.exports = {
