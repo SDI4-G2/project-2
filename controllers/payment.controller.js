d81512489f0243ca40284d5001a71c9517627dd5
Laily97 <simonngchinsun@gmail.com>
2022-06-28 22:47:58 +0800

    stripe intent

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,6 +1,31 @@
 const paymentService = require('../services/payment.service');
 
+const PUBLISHABLE_KEY = "pk_test_51LEjuRAfZxkm95kPjgIpndtLpAKOkhmJQJg2pf9iAglCHxqlLg20DPosvYrcgydkHTCr5tPECLzPalnjHRnEtBdE00oK5ZHOmO";
+const SECRET_KEY = "sk_test_51LEjuRAfZxkm95kPGxK6yaYR2VKhIeZwkYXhLGGc1q75E5G4hjcTLHV3lrr3cdyViQ1OD2GpwDbT6RhHdfht0Dns00DLi0jihC";
+import Stripe from "stripe";
+
+//Confirm the API version from stripe dashboard
+const stripe = Stripe(SECRET_KEY, { apiVersion: "2020-08-27" });
+
 class PaymentController {
+    async createPaymentIntent(req, res) {
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+              amount: 1099, //lowest denomination of particular currency
+              currency: "usd",
+              payment_method_types: ["card"], //by default
+            });
+        
+            const clientSecret = paymentIntent.client_secret;
+        
+            res.json({
+              clientSecret: clientSecret,
+            });
+        } catch (e) {
+            res.json({ error: e.message });
+        }
+    }
+
     async insertPayment(req, res) {
         const paymentJson = req.body;
         
--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -16,6 +16,7 @@ router.use('/', (req, res, next) => {
     next();
 })
 
+router.post('/create-payment-intent', paymentController.createPaymentIntent);
 router.post('/payment', paymentController.insertPayment);
 
 module.exports = router;
