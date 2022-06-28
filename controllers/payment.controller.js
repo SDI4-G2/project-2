b27bd2d313c27ac0c08a0dddaf78d63772b59e1b
Laily97 <simonngchinsun@gmail.com>
2022-06-28 22:56:10 +0800

    Update payment.controller.js

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -2,7 +2,8 @@ const paymentService = require('../services/payment.service');
 
 const PUBLISHABLE_KEY = "pk_test_51LEjuRAfZxkm95kPjgIpndtLpAKOkhmJQJg2pf9iAglCHxqlLg20DPosvYrcgydkHTCr5tPECLzPalnjHRnEtBdE00oK5ZHOmO";
 const SECRET_KEY = "sk_test_51LEjuRAfZxkm95kPGxK6yaYR2VKhIeZwkYXhLGGc1q75E5G4hjcTLHV3lrr3cdyViQ1OD2GpwDbT6RhHdfht0Dns00DLi0jihC";
-import Stripe from "stripe";
+
+const Stripe = require('stripe');
 
 //Confirm the API version from stripe dashboard
 const stripe = Stripe(SECRET_KEY, { apiVersion: "2020-08-27" });
