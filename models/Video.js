e3f31c2ff6450aae647821bdd972e4835b9dbcd7
Ewen <ewenlke@hotmail.com>
2022-06-30 23:55:53 +0800

    addition thumbnail, title, text columns

--- models/Video.js
+++ models/Video.js
@@ -1,40 +1,50 @@
-const {DataTypes, Model} = require('sequelize');
-const {sequelize} = require('./DBConn');
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('./DBConn');
 const Category = require('./Category');
 
 class Videos extends Model {}
-Videos.init (
-    {
-        videoid: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        categoryid: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        url: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        freeToView: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-            field: 'free_to_view'
-
-        }
+Videos.init(
+  {
+    videoid: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        modelName: 'Videos',
-        tableName: 'videos'
-    }
-
+    categoryid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    freeToView: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      field: 'free_to_view',
+    },
+    thumbnails: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Videos',
+    tableName: 'videos',
+  }
 );
 
 Videos.belongsTo(Category, {
-    foreignKey: 'categoryid'
+  foreignKey: 'categoryid',
 });
 
 module.exports = Videos;
